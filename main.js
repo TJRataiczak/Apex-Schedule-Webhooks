8be7ca644d7d3a62a3fe94928e9593ebc97ac3ae
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-08-21 22:04:23 -0400

    changes

---
+++ main.js
@@ -0,0 +1,4 @@
+const webhookUrl = "https://discord.com/api/webhooks/1143361330791858206/Z8jxGJHDP8ohI8_LJCCOuwuq5NvFjOhPZzSg8vyMhBKN71NVe_qcqyBCqZ5ZYoRXZRPl"
+
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+const { webhookId, webhookToken } = require('./config.json');
