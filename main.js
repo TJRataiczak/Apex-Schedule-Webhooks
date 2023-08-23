b50850eda1bc8990308f8b4a24e6f72c27d0e266
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-08-23 13:52:35 -0400

    changes

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const webhookUrl = "https://discord.com/api/webhooks/1143361330791858206/Z8jxGJHDP8ohI8_LJCCOuwuq5NvFjOhPZzSg8vyMhBKN71NVe_qcqyBCqZ5ZYoRXZRPl"
-const mydata = require("./json/events.json");
+const myData = require("./json/events.json");
 const todaysDate = new Date();
+let daysOfWeek = [];
+let weeklyEvents = {};
 
-// console.log(todaysDate.setDate(todaysDate.getDate()+1));
-
-console.log(mydata.events.length);
-
-for (let i = 0; i < mydata.events.length; i++) {
-    console.log(mydata.events[i]);
+function makeDateFromDay (weekday, daysOfWeek) {
+    
 }
+
+// Put days of the week into an array to be checked later
+for (let i = 0; i < 7; i++) {
+    if (i !== 0) {
+        let newDate = todaysDate.setDate(todaysDate.getDate()+1);
+        daysOfWeek.push(newDate);
+}
+    else {
+        daysOfWeek.push(todaysDate.getTime());
+    }
+}
+
+// for (let i = 0; i < myData["reoccuring-events"].length; i++) {
+//     switch (myData["reoccuring-events"][i]["event-day-of-week"]) {
+//         case 0:
+
+//     }
+// }
+
+// console.log(new Date(daysOfWeek[0]).setHours(18 , 30, 0, 0));
