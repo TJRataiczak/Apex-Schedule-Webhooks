38324d65ae91d56cb7930fc345192732a3787162
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-08-23 19:52:53 -0400

    changes

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const webhookUrl =
   "https://discord.com/api/webhooks/1143361330791858206/Z8jxGJHDP8ohI8_LJCCOuwuq5NvFjOhPZzSg8vyMhBKN71NVe_qcqyBCqZ5ZYoRXZRPl";
 const myData = require("./json/events.json");
-let todaysDate = new Date().setHours(0,0,0,0);
+let todaysDate = new Date().setHours(0, 0, 0, 0);
 let daysOfWeek = [];
 let weeklyEvents = {};
 
-function putEventInDictionary (event, weekday, hours, minutes) {
-    weeklyEvents[event] = {eventName: `${event}`,
-                        eventDate: `${new Date(daysOfWeek[weekday]).setHours(hours, minutes, 0, 0)}`};
+function putEventInDictionary(event, weekday, hours, minutes) {
+  weeklyEvents[event] = {
+    eventName: `${event}`,
+    eventDate: `${new Date(daysOfWeek[weekday]).setHours(hours, minutes, 0, 0)}`
+  };
 }
 
 // Put days of the week into an array to be referenced later
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 8; i++) {
   if (i !== 0) {
     let newDate = new Date(todaysDate);
     newDate.setDate(newDate.getDate() + i)
@@ -23,6 +25,7 @@ for (let i = 0; i < 7; i++) {
 
 //Puts all reoccuring events from JSON into a dictionary
 for (let i = 0; i < myData["reoccuring-events"].length; i++) {
+
   let currentTime = myData["reoccuring-events"][i]["event-time"].split(" ");
 
   switch (myData["reoccuring-events"][i]["event-day-of-week"]) {
@@ -30,22 +33,22 @@ for (let i = 0; i < myData["reoccuring-events"].length; i++) {
       putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 6, Number(currentTime[0]), Number(currentTime[1]));
       break;
     case 1:
-        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 0, Number(currentTime[0]), Number(currentTime[1]));
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 0, Number(currentTime[0]), Number(currentTime[1]));
       break;
     case 2:
-        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 1, Number(currentTime[0]), Number(currentTime[1]));
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 1, Number(currentTime[0]), Number(currentTime[1]));
       break;
     case 3:
-        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 2, Number(currentTime[0]), Number(currentTime[1]));
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 2, Number(currentTime[0]), Number(currentTime[1]));
       break;
     case 4:
-        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 3, Number(currentTime[0]), Number(currentTime[1]));
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 3, Number(currentTime[0]), Number(currentTime[1]));
       break;
     case 5:
-        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 4, Number(currentTime[0]), Number(currentTime[1]));
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 4, Number(currentTime[0]), Number(currentTime[1]));
       break;
     case 6:
-        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 5, Number(currentTime[0]), Number(currentTime[1]));
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 5, Number(currentTime[0]), Number(currentTime[1]));
       break;
   }
 }
