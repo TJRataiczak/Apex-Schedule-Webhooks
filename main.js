f0e00a2a5d5a0b0dac7666d0992221db8b22a604
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-08-23 15:09:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,55 @@
-const webhookUrl = "https://discord.com/api/webhooks/1143361330791858206/Z8jxGJHDP8ohI8_LJCCOuwuq5NvFjOhPZzSg8vyMhBKN71NVe_qcqyBCqZ5ZYoRXZRPl"
+const webhookUrl =
+  "https://discord.com/api/webhooks/1143361330791858206/Z8jxGJHDP8ohI8_LJCCOuwuq5NvFjOhPZzSg8vyMhBKN71NVe_qcqyBCqZ5ZYoRXZRPl";
 const myData = require("./json/events.json");
-const todaysDate = new Date();
+let todaysDate = new Date().setHours(0,0,0,0);
 let daysOfWeek = [];
 let weeklyEvents = {};
 
-function makeDateFromDay (weekday, daysOfWeek) {
-    
+function putEventInDictionary (event, weekday, hours, minutes) {
+    weeklyEvents[event] = {eventName: `${event}`,
+                        eventDate: `${new Date(daysOfWeek[weekday]).setHours(hours, minutes, 0, 0)}`};
 }
 
-// Put days of the week into an array to be checked later
+// Put days of the week into an array to be referenced later
 for (let i = 0; i < 7; i++) {
-    if (i !== 0) {
-        let newDate = todaysDate.setDate(todaysDate.getDate()+1);
-        daysOfWeek.push(newDate);
+  if (i !== 0) {
+    let newDate = new Date(todaysDate);
+    newDate.setDate(newDate.getDate() + i)
+    daysOfWeek.push(newDate.getTime());
+  } else {
+    daysOfWeek.push(todaysDate);
+  }
 }
-    else {
-        daysOfWeek.push(todaysDate.getTime());
-    }
+
+//Puts all reoccuring events from JSON into a dictionary
+for (let i = 0; i < myData["reoccuring-events"].length; i++) {
+  let currentTime = myData["reoccuring-events"][i]["event-time"].split(" ");
+
+  switch (myData["reoccuring-events"][i]["event-day-of-week"]) {
+    case 0:
+      putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 6, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+    case 1:
+        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 0, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+    case 2:
+        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 1, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+    case 3:
+        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 2, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+    case 4:
+        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 3, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+    case 5:
+        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 4, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+    case 6:
+        putEventInDictionary(myData["reoccuring-events"][i]["event-name"], 5, Number(currentTime[0]), Number(currentTime[1]));
+      break;
+  }
 }
 
-// for (let i = 0; i < myData["reoccuring-events"].length; i++) {
-//     switch (myData["reoccuring-events"][i]["event-day-of-week"]) {
-//         case 0:
 
-//     }
-// }
-
-// console.log(new Date(daysOfWeek[0]).setHours(18 , 30, 0, 0));
+console.log(weeklyEvents);
+console.log(daysOfWeek);
