379e94235b1ff16c3d3b74c6b410b0926c405ae1
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-08-23 10:45:32 -0400

    changes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const webhookUrl = "https://discord.com/api/webhooks/1143361330791858206/Z8jxGJHDP8ohI8_LJCCOuwuq5NvFjOhPZzSg8vyMhBKN71NVe_qcqyBCqZ5ZYoRXZRPl"
-
 const mydata = require("./json/events.json");
+const todaysDate = new Date();
 
+// console.log(todaysDate.setDate(todaysDate.getDate()+1));
 
-console.log(mydata.events);
+console.log(mydata.events.length);
+
+for (let i = 0; i < mydata.events.length; i++) {
+    console.log(mydata.events[i]);
+}
